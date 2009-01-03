8498a9105bdf007c17d44b0ad70535c17d45a226
Corey Haines <coreyhaines@gmail.com>
2009-01-03 06:01:40 -0500

    continued to fill in skeleton a bit more

--- lib/sudoku_solver.rb
+++ lib/sudoku_solver.rb
@@ -12,7 +12,15 @@ class SudokuSolver
       board.fill_in_rows_with_single_missing_digit!
       # board.fill_in_columns_with_single_missing_digit!
       # board.fill_in_squares_with_single_missing_digit!
+      # board.fill_in_pairs_in_rows!
+      # board.fill_in_pairs_in_columns!
+      # board.fill_in_pairs_in_squares!
+      # board.fill_in_triplets_in_rows!
+      # board.fill_in_triplets_in_columns!
+      # board.fill_in_triplets_in_squares!
+      # ...
     end
+    #board.brute_force_finish! unless board.completed?
     board
   end
   
